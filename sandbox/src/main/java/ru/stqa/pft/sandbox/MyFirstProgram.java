ae0a997b3bc58b0cde24e32b8eeb8861b549cc3a
antond <antond45456@gmail.com>
2021-09-18 16:31:25 +0300

    task 2

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -1,8 +1,20 @@
 package ru.stqa.pft.sandbox;
 
-    public class MyFirstProgram{
+import java.awt.*;
+
+public class MyFirstProgram{
 
         public static void main (String[] args){
+
+          Point t = new Point(8, 4);
+          System.out.println("расстояние между точками " + t.distance());
+
             System.out.println("Hello world!");
+
+            System.out.println();
         }
+
+
+
+
     }
