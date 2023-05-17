19f8347084246d4f46e8d7ee88ffb26aee4d27c7
New_pc <antond45456@gmail.com>
2023-05-18 00:21:33 +0300

    поменял файл для теста

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -7,9 +7,9 @@ public class MyFirstProgram{
         public static void main (String[] args){
 
           Point t = new Point(8, 4);
-          System.out.println("расстояние между точками " + t.distance());
+          System.out.println("расстояние между точками 1 и 3 " + t.distance());
 
-            System.out.println("Hello world!");
+            System.out.println("Hello world!a");
 
             System.out.println();
         }
